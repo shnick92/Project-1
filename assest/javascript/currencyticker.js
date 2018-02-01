415a13b52f7650baecc66d3774fbcba4379331f0
Nick Stone <nicholas.a.stone1992@gmail.com>
2018-01-31 18:57:25 -0600

    .

---
+++ assest/javascript/currencyticker.js
@@ -0,0 +1,25 @@
+// Currency Ticker
+var currencyURL = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,BCH,LTC&tsyms=USD";
+
+    $(function(){
+        setInterval(oneSecondFunction, 1);
+    });
+
+    function oneSecondFunction() {
+        var currencyURL = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,BCH,LTC&tsyms=USD";
+
+        $.ajax({
+            url: currencyURL,
+            method: "GET"
+        })
+        .then(function(response) {
+
+
+            $("#BTC").text(response.BTC.USD);
+            $("#ETH").text(response.ETH.USD);
+            $("#XRP").text(response.XRP.USD);
+            $("#BCH").text(response.BCH.USD);
+            $("#LTC").text(response.LTC.USD);
+
+        });
+    };
